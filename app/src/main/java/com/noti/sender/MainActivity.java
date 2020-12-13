49afc9bb89fe01a359999adda9b8cec9be874127
choiman1559 <cuj1559@gmail.com>
2020-12-13 22:14:31 +0900

    fixed some bugs

--- app/src/main/java/com/noti/sender/MainActivity.java
+++ app/src/main/java/com/noti/sender/MainActivity.java
@@ -6,7 +6,6 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ComponentName;
@@ -16,23 +15,11 @@ import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -59,10 +46,16 @@ public class MainActivity extends AppCompatActivity {
                 new GetReleaseTask(this, status2, false).execute();
             } else {
                 startMainActivity(this);
-                finish();
+                ExitActivity.exitApplication(this);
             }
         } else {
-            new GetReleaseTask(this, status2, true).execute();
+            if(isOnline()) new GetReleaseTask(this, status2, true).execute();
+            else {
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.setNegativeButton("Close", (d,w) -> ExitActivity.exitApplication(this));
+                alert.setCancelable(false).setTitle("Error").setMessage("please check Internet connection and try again!");
+                alert.show();
+            }
         }
     }
 
@@ -125,94 +118,5 @@ public class MainActivity extends AppCompatActivity {
             return ni != null && ni.isConnected();
         }
         return false;
-    }
-
-    @SuppressLint("StaticFieldLeak")
-    protected static class GetReleaseTask extends AsyncTask<Void, Void, JSONArray> {
-        Activity context;
-        ProgressBar progressBar;
-        boolean isNewInstall;
-
-        GetReleaseTask(Activity context, ProgressBar progressBar, boolean value) {
-            this.context = context;
-            this.progressBar = progressBar;
-            this.isNewInstall = value;
-        }
-
-        @Override
-        protected JSONArray doInBackground(Void... params) {
-            String str = "https://api.github.com/repos/choiman1559/NotiSender/releases";
-            URLConnection urlConn;
-            BufferedReader bufferedReader = null;
-            try {
-                URL url = new URL(str);
-                urlConn = url.openConnection();
-                bufferedReader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
-
-                StringBuilder stringBuffer = new StringBuilder();
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    stringBuffer.append(line);
-                }
-
-                return new JSONArray(stringBuffer.toString());
-            } catch (Exception ex) {
-                Log.e("App", "yourDataTask", ex);
-                return null;
-            } finally {
-                if (bufferedReader != null) {
-                    try {
-                        bufferedReader.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-
-        @Override
-        protected void onPostExecute(JSONArray response) {
-            if (response != null) {
-                try {
-                    JSONObject obj = response.getJSONObject(0);
-                    String latestVersion = obj.getString("tag_name");
-                    if (isNewInstall) {
-                        AlertDialog.Builder alert = new AlertDialog.Builder(context);
-                        alert
-                                .setCancelable(false)
-                                .setTitle(context.getString(R.string.dialog_download_title))
-                                .setMessage(context.getString(R.string.dialog_download_message))
-                                .setNegativeButton("Cancel",(d,w) -> ExitActivity.exitApplication(context))
-                                .setPositiveButton("Download",(d,w) -> {
-                            ((TextView) context.findViewById(R.id.status1)).setText(context.getString(R.string.main_download));
-                            new DownloadTask(context, progressBar).execute("https://github.com/choiman1559/NotiSender/releases/download/" + latestVersion + "/app-release.apk");
-                        }).show();
-                    } else {
-                        PackageManager pm = context.getPackageManager();
-                        String localVersion = pm.getPackageInfo("com.noti.main", 0).versionName.replace(" Beta", "");
-                        Version v1 = new Version(latestVersion);
-                        Version v2 = new Version(localVersion);
-
-                        if (v1.compareTo(v2) > 0) {
-                            AlertDialog.Builder alert = new AlertDialog.Builder(context);
-                            alert
-                                    .setCancelable(false)
-                                    .setTitle(context.getString(R.string.dialog_update_title))
-                                    .setMessage(context.getString(R.string.dialog_update_message))
-                                    .setNegativeButton("Cancel",(d,w) -> ExitActivity.exitApplication(context))
-                                    .setPositiveButton("Update",(d,w) -> {
-                                        ((TextView) context.findViewById(R.id.status1)).setText(context.getString(R.string.main_updating));
-                                        new DownloadTask(context, progressBar).execute("https://github.com/choiman1559/NotiSender/releases/download/" + latestVersion + "/app-release.apk");
-                                    }).show();
-                        } else {
-                            startMainActivity(context);
-                            ExitActivity.exitApplication(context);
-                        }
-                    }
-                } catch (JSONException | PackageManager.NameNotFoundException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }
     }
 }
